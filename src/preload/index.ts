b255f76d86bd01cfc441433f0320f8975aafb77e
Jacky Yang <jackyyangxxx@gmail.com>
2026-05-27 11:04:26 +0800

    fix: add missing IPC handlers to preload (getTodos, addTodo, toggleTodo, deleteTodo, updateSortOrder, onTriggerInput)

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,6 +1,7 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 const api = {
+  // Window controls
   closeSettingsWindow: () => ipcRenderer.invoke('settings:window:close'),
   onRefreshPetImages: (callback: () => void) => {
     const listener = () => callback();
@@ -12,9 +13,23 @@ const api = {
     ipcRenderer.on('open-settings', listener);
     return () => ipcRenderer.removeListener('open-settings', listener);
   },
+  onTriggerInput: (callback: () => void) => {
+    const listener = () => callback();
+    ipcRenderer.on('trigger-input', listener);
+    return () => ipcRenderer.removeListener('trigger-input', listener);
+  },
+
+  // Pet images
   getPetImages: () => ipcRenderer.invoke('pets:getImages'),
   uploadPetImage: (state: string, filePath: string) =>
     ipcRenderer.invoke('pets:uploadImage', state, filePath),
+
+  // Todos
+  getTodos: () => ipcRenderer.invoke('todos:getAll'),
+  addTodo: (input: { content: string }) => ipcRenderer.invoke('todos:add', input),
+  toggleTodo: (id: string) => ipcRenderer.invoke('todos:toggle', id),
+  deleteTodo: (id: string) => ipcRenderer.invoke('todos:delete', id),
+  updateSortOrder: (ids: string[]) => ipcRenderer.invoke('todos:updateSortOrder', ids),
 };
 
 contextBridge.exposeInMainWorld('todoAPI', api);
