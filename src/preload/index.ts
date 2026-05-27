e55a97d3892c38cae4e42613ad287cc0664bbe99
Jacky Yang <jackyyangxxx@gmail.com>
2026-05-27 08:47:45 +0800

    fix: add missing todoAPI methods to preload (getPetImages, uploadPetImage, onOpenSettings)

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -7,6 +7,14 @@ const api = {
     ipcRenderer.on('refresh-pet-images', listener);
     return () => ipcRenderer.removeListener('refresh-pet-images', listener);
   },
+  onOpenSettings: (callback: () => void) => {
+    const listener = () => callback();
+    ipcRenderer.on('open-settings', listener);
+    return () => ipcRenderer.removeListener('open-settings', listener);
+  },
+  getPetImages: () => ipcRenderer.invoke('pets:getImages'),
+  uploadPetImage: (state: string, filePath: string) =>
+    ipcRenderer.invoke('pets:uploadImage', state, filePath),
 };
 
 contextBridge.exposeInMainWorld('todoAPI', api);
